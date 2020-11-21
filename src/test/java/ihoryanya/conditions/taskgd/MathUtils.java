f89f655103235e3ba7799ef0d48d9e631125dd4f
lolus <lolus@DESKTOP-Q0MMUSH>
2020-11-21 15:10:07 +0200

    First Task

---
+++ src/test/java/ihoryanya/conditions/taskgd/MathUtils.java
@@ -0,0 +1,15 @@
+package ihoryanya.conditions.taskgd;
+
+public class MathUtils {
+    public static long getFactorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Invalid argument number = " + number);
+        }
+        long factorial = 1;
+        while (number > 1) {
+            factorial *= number;
+            number--;
+        }
+        return factorial;
+    }
+}
