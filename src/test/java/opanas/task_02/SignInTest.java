425fd2c1b657405a97b2462cef27db0d91d2aabb
panas224 <panas.oleh2019@gmail.com>
2020-11-26 11:20:02 +0200

    renamed variables due to code convention

--- src/test/java/opanas/task_02/SignInTest.java
+++ src/test/java/opanas/task_02/SignInTest.java
@@ -11,7 +11,6 @@ public class SignInTest {
 
     private WebDriver driver;
     private final String baseUrl = "https://ita-social-projects.github.io/GreenCityClient/#/welcome";
-    private static int delay=10;
     @BeforeClass
     public void beforeClass() {
         driver = new ChromeDriver();
@@ -37,32 +36,32 @@ public class SignInTest {
     }
     @Test(description = "In this test we will input the valid data in all fields of Sign in form and look if we will be redirect to our personal account")
     public void verifyValidData()  {
-        WebElement LogInEmail=driver.findElement(By.id("email"));
-        LogInEmail.click();
-        LogInEmail.sendKeys("xdknxusqvjeovowpfk@awdrt.com");
-        WebElement LogInPassword=driver.findElement(By.id("password"));
-        LogInPassword.click();
-        LogInPassword.sendKeys(System.getenv().get("valid_password"));
-        WebElement button=driver.findElement(By.xpath("//*[@type='submit']"));
-        button.click();
+        WebElement logInEmail=driver.findElement(By.id("email"));
+        logInEmail.click();
+        logInEmail.sendKeys("xdknxusqvjeovowpfk@awdrt.com");
+        WebElement logInPassword=driver.findElement(By.id("password"));
+        logInPassword.click();
+        logInPassword.sendKeys(System.getenv().get("valid_password"));
+        WebElement buttonSignIn=driver.findElement(By.xpath("//*[@type='submit']"));
+        buttonSignIn.click();
         Assert.assertEquals(driver.findElement(By.xpath("//*[@class='tertiary-global-button']")).getText(),"you can buy eco-bags here");
     }
     @Test(description = "Fill in password with incorrect data")
     private void verifyInvalidPassword() {
-        WebElement LogInEmail=driver.findElement(By.id("email"));
-        LogInEmail.click();
-        LogInEmail.sendKeys("xdknxusqvjeovowpfk@awdrt.com");
-        WebElement LogInPassword=driver.findElement(By.id("password"));
-        LogInPassword.click();
-        LogInPassword.sendKeys("55464");
+        WebElement loginEmail=driver.findElement(By.id("email"));
+        loginEmail.click();
+        loginEmail.sendKeys("xdknxusqvjeovowpfk@awdrt.com");
+        WebElement logInPassword=driver.findElement(By.id("password"));
+        logInPassword.click();
+        logInPassword.sendKeys("55464");
         driver.findElement(By.xpath(".//*[@class='or-use-google']")).click();
         Assert.assertEquals(driver.findElement(By.xpath("//*[@class='validation-password-error ng-star-inserted']")).getText(),"Password must be at least 8 characters long");
     }
     @Test(description = "Fill in email with incorrect data")
     private void verifyInvalidEmail() {
-        WebElement LogInEmail=driver.findElement(By.id("email"));
-        LogInEmail.click();
-        LogInEmail.sendKeys("xdknxusqvjeovowpfk.awdrt.com");
+        WebElement logInEmail=driver.findElement(By.id("email"));
+        logInEmail.click();
+        logInEmail.sendKeys("xdknxusqvjeovowpfk.awdrt.com");
         driver.findElement(By.xpath(".//*[@class='or-use-google']")).click();
         Assert.assertEquals(driver.findElement(By.xpath("//*[@class='validation-email-error ng-star-inserted']")).getText(),"Please check that your e-mail address is indicated correctly");
     }
